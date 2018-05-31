9a0c01f8dcd0366cef8ac304735a45c2b8a07aa6
Yeisson <yeisson@gmail.com>
2018-05-31 00:30:17 -0500

    Autorizado de peticiones CORS y corregido token en la creaciòn de usuario

--- routes/usuario.js
+++ routes/usuario.js
@@ -103,7 +103,7 @@ app.put('/:id', mdAutenticacion.verificaToken, (req, res) => {
 // ============================================
 // Crear un nuevo usuario
 // ============================================
-app.post('/', mdAutenticacion.verificaToken, (req, res) => {
+app.post('/', (req, res) => {
 
     var body = req.body;
 
