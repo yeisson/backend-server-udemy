9d09fef744bed82f1b21f48b62e1c92d705ea9e2
Yeisson <yeisson@gmail.com>
2018-06-11 17:11:16 -0500

    modificacion login

--- routes/login.js
+++ routes/login.js
@@ -12,6 +12,22 @@ var CLIENT_ID = require('../config/config').CLIENT_ID;
 const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client(CLIENT_ID);
 
+var mdAutenticacion = require('../middlewares/autenticacion');
+
+// =====================================
+// Renovar Token
+// =====================================
+app.get('/renuevatoken', mdAutenticacion.verificaToken, (req, res) => {
+
+    var token = jwt.sign({ usuario: req.usuario }, SEED, { expiresIn: 14400 }); //4 horas
+
+    res.status(200).json({
+        ok: true,
+        token: token
+    });
+
+});
+
 // =====================================
 // Autenticación Google
 // =====================================
